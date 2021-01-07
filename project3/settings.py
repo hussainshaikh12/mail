2d6d62e7b6811f68bd2af7ad3528efe6859e319b
Hussain Shaikh <zakirroll@gmail.com>
2021-01-07 21:54:17 +0530

    changed settings.py for working on localhost

--- project3/settings.py
+++ project3/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cs50gmail.herokuapp.com','127.0.0.1']
 
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # for serving static files on heroku
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,7 +122,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+# for serving static files on heroku   
+# STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles') 
 
 STATIC_URL = '/static/'
 
